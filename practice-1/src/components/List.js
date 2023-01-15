6b30cbf1badfa49c6b430e5dde13bc181dd48c41
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-01-15 17:16:41 +0600

    unique id generate

---
+++ practice-1/src/components/List.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {v4 as uuidv4} from 'uuid'
+
+const info = [
+    {
+        uuid: uuidv4(),
+        name: "Sabbir",
+        id: 1802162
+    },
+    {
+        uuid: uuidv4(),
+        name: "Mosh",
+        id: 1702160
+    },
+    {
+        uuid: uuidv4(),
+        name: "Hasibul",
+        id: 1802107
+    },
+    {
+        uuid: uuidv4(),
+        name: "Shahinur",
+        id: 1802111
+    }
+]
+
+
+const List = () => {
+    return (
+        <div>
+          {info.map((item) => { 
+            const {name, id, uuid} = item;
+            return(
+                <div key={uuid} >
+                    <h3>{name}</h3>
+                    <h3>{id}</h3>
+                </div>
+            )
+          })}
+        </div>
+       
+    );
+};
+
+export default List;
