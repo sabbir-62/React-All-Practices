d35c3874f617414dbb32ca9ef96f6126904d511f
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-01-20 01:54:00 +0600

    react icon uses

---
+++ practice-3/src/App.js
@@ -0,0 +1,14 @@
+import './app.css';
+import { FaAws, FaAmazonPay } from "react-icons/fa";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Sabbir</p>
+      <FaAws className='icon'/>
+      <FaAmazonPay className='icon'/>
+    </div>
+  );
+}
+
+export default App;
